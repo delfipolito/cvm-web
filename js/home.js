b3fab2b42e9f9014951d0ea8dbe446eafbc85ef3
Delfina <delfinapolito@gmail.com>
2016-01-20 12:16:19 -0200

    first aproach to cvm web

---
+++ js/home.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	 $(".button-collapse").sideNav();
+
+});
+
+
+
+
+$(window).load(function(){
+   $('body').css({visibility: "visible"}).fadeTo(2000, 1);
+});
---
+++ js/translation.js
@@ -0,0 +1,11 @@
+var translations = {}
+
+translations.es = {
+   slogan: "Nuestros 10 años en el mercado de los servicios de alta tecnología informática nos posicionan como proveedores de primer nivel y garantizan nuestro conocimiento de las distintas realidades y tipos de empresas.",
+   
+};
+
+translations.en = {
+   slogan: "Hardware and Software Development",
+   
+};
